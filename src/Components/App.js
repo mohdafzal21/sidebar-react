8b142b7f7c39e58e13bd599fe8a3b0945b0c1188
mohdafzal21 <mohammedafzal94@gmail.com>
2019-03-28 19:56:23 +0530

    react sidebar

---
+++ src/Components/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { NotFound } from './Errors'
+import Writers from './Writers'
+import Layout from './Layout'
+
+
+
+export default class extends Component {
+  state = {
+    writers: [
+      
+    ]
+  }
+
+  async componentDidMount() {
+    const writers = await (await fetch('http://localhost:3004/writers?_embed=texts')).json()
+
+    this.setState({ writers })
+  }
+
+  render() {
+    const { writers } = this.state
+
+    return <BrowserRouter>
+      <Layout writers={writers}>
+        <Switch>
+          <Route exact path="/" render={() => <div>Home</div>} />
+          <Route path="/writers" render={
+            props => <Writers {...props} writers={writers} />
+          } />
+          <Route component={NotFound} />
+        </Switch>
+      </Layout>
+    </BrowserRouter>
+  }
+}
